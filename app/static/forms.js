353bdc87d1b4a9cb5ad6595b7f80099d4066cb16
Robert Gingras <developer@three-point-five.dev>
2024-01-01 11:31:35 -0500

    refactor

---
+++ app/static/forms.js
@@ -0,0 +1,132 @@
+/* eslint-env browser */
+const causeAlert = (message) => {
+  const alert = document.createElement('div')
+  alert.classList.add('alert', 'alert-warning', 'alert-dismissible', 'fade', 'show')
+  alert.innerHTML = message
+  alert.role = "alert"
+
+  const closeButton = document.createElement('button')
+  closeButton.type = "button"
+  closeButton.classList.add("btn-close")
+  closeButton.dataset.bsDismiss = "alert"
+  closeButton.ariaLabel = "Close"
+
+  alert.appendChild(closeButton)
+  document.body.insertBefore(alert, document.body.firstChild)
+}
+
+module.exports = {
+  /**
+   * Intercepts submit events from the tropes search box and makes the AJAX call to update the filter list
+   */
+  tropes: function(event) {
+    event.preventDefault()
+
+    // disable form controls while loading
+    // doesn't need to be removed on success since the new html will replace it
+    for (const t of document.querySelectorAll('[name="t"]')) {
+      t.setAttribute('disabled', 'true')
+      t.parentElement.classList.add('placeholder-glow')
+      document.querySelector(`[for="${t.id}"]`).classList.add('placeholder')
+    }
+
+    // submit the form
+    const tropeSearchQuery = document.getElementById('tsq').value
+    const params = Array.from(document.querySelectorAll('[name="t"], [name="n]'))
+      .filter(e => e.checked).map(e => `${e.name}=${e.value}`).join('&')
+    fetch(`/tropes?${params}&tsq=${tropeSearchQuery}`).then((response) => {
+      if (response.ok) {
+        return response.text()
+      }
+      throw new Error(`${response.status} ${response.statusText}`)
+    }).then((body) => {
+      document.getElementById('trope-filters').innerHTML = body
+    }).catch((err) => {
+      causeAlert(err.message)
+
+      // remove placeholder values from form
+      for (const t of document.querySelectorAll('[name="t"]')) {
+        t.removeAttribute('disabled')
+        t.parentElement.classList.remove('placeholder-glow')
+        document.querySelector(`[for="${t.id}"]`).classList.remove('placeholder')
+      }
+    })
+  },
+
+  /**
+   * Intercepts submit event on the namespaces search, loads the search results
+   */
+  namespaces: function(event) {
+    event.preventDefault()
+
+    // disable form controls while loading
+    // doesn't need to be removed on success since the new html will replace it
+    for (const n of document.querySelectorAll('[name="n"]')) {
+      n.setAttribute('disabled', 'true')
+      n.parentElement.classList.add('placeholder-glow')
+      document.querySelector(`[for="${n.id}"]`).classList.add('placeholder')
+    }
+
+    const namespaceSearchQuery = document.getElementById('nsq').value
+    const namespaces = Array.from(document.querySelectorAll('[name="n"]'))
+      .filter(e => e.checked).map(e => `${e.name}=${e.value}`).join('&')
+    fetch(`/articlecount?nsq=${namespaceSearchQuery}&${namespaces}`).then(response => {
+      if (response.ok) {
+        return response.text()
+      }
+      throw new Error(`${response.status} ${response.statusText}`)
+    }).then(body => {
+      document.getElementById('namespace-filters').innerHTML = body
+    }).catch((err) => {
+      causeAlert(err.message)
+
+      for (const n of document.querySelectorAll('[name="n"]')) {
+        n.removeAttribute('disabled')
+        n.parentElement.classList.remove('placeholder-glow')
+        document.querySelector(`[for="${n.id}"]`).classList.remove('placeholder')
+      }
+    })
+  },
+
+  /**
+   * updates the history and loads the new page of results. Should be callable from either form submit or link follow
+   */
+  search: function(event) {
+    event.preventDefault()
+
+    const formdata = new FormData(document.getElementById('search'))
+    const params = Array.from(formdata).map(f => f.join('=')).join('&')
+
+    // update the browser history
+    history.pushState({}, '', `/search?${params}`)
+
+    // set the placeholders
+    for (const e of document.querySelectorAll('.twikilink')) {
+      e.classList.add('placeholder', 'disabled')
+      e.ariaDisabled = true
+      e.setAttribute('tabindex', -1)
+      e.parentElement.classList.add('placeholder-glow')
+    }
+
+    // submit the form
+    fetch(`/results?${params}`).then((response) => {
+      if (response.ok) {
+        return response.text()
+      }
+
+      throw new Error(`${response.status} ${response.statusText}`)
+    }).then((body) => {
+      document.getElementById('results').innerHTML = body
+    }).catch((err) => {
+      causeAlert(err.message)
+
+      // remove the placeholders
+      for (const e of document.querySelectorAll('.twikilink')) {
+        e.classList.remove('placeholder', 'disabled')
+        e.ariaDisabled = false
+        e.removeAttribute('tabindex')
+        e.parentElement.classList.remove('placeholder-glow')
+      }
+    })
+  }
+}
--- app/static/lazyload.js
+++ app/static/lazyload.js
@@ -1,3 +1,5 @@
+const forms = require('./forms.js')
+
 /* eslint-env browser */
 window.onload = function() {
   // replace the "tropes" form control with its own form
@@ -9,39 +11,7 @@ window.onload = function() {
   for (const child of tropesForm.querySelectorAll('[form]')) {
     child.removeAttribute('form')
   }
-  tropesForm.addEventListener('submit', function(event) {
-    event.preventDefault()
-
-    // disable form controls while loading
-    // doesn't need to be removed on success since the new html will replace it
-    for (const t of document.querySelectorAll('[name="t"]')) {
-      t.setAttribute('disabled', 'true')
-      t.parentElement.classList.add('placeholder-glow')
-      document.querySelector(`[for="${t.id}"]`).classList.add('placeholder')
-    }
-
-    // submit the form
-    const tropeSearchQuery = document.getElementById('tsq').value
-    const params = Array.from(document.querySelectorAll('[name="t"], [name="n]'))
-      .filter(e => e.checked).map(e => `${e.name}=${e.value}`).join('&')
-    fetch(`/tropes?${params}&tsq=${tropeSearchQuery}`).then((response) => {
-      if (response.ok) {
-        return response.text()
-      }
-      throw new Error(`${response.status} ${response.statusText}`)
-    }).then((body) => {
-      document.getElementById('trope-filters').innerHTML = body
-    }).catch((err) => {
-      causeAlert(err.message)
-
-      // remove placeholder values from form
-      for (const t of document.querySelectorAll('[name="t"]')) {
-        t.removeAttribute('disabled')
-        t.parentElement.classList.remove('placeholder-glow')
-        document.querySelector(`[for="${t.id}"]`).classList.remove('placeholder')
-      }
-    })
-  })
+  tropesForm.addEventListener('submit', forms.tropes)
   tropesDiv.replaceWith(tropesForm)
 
   // do the same with the "namespaces" controls
@@ -52,79 +22,12 @@ window.onload = function() {
   for (const child of namespacesForm.querySelectorAll('[form]')) {
     child.removeAttribute('form')
   }
-  namespacesForm.addEventListener('submit', function(event) {
-    event.preventDefault()
-
-    // disable form controls while loading
-    // doesn't need to be removed on success since the new html will replace it
-    for (const n of document.querySelectorAll('[name="n"]')) {
-      n.setAttribute('disabled', 'true')
-      n.parentElement.classList.add('placeholder-glow')
-      document.querySelector(`[for="${n.id}"]`).classList.add('placeholder')
-    }
-
-    const namespaceSearchQuery = document.getElementById('nsq').value
-    const namespaces = Array.from(document.querySelectorAll('[name="n"]'))
-      .filter(e => e.checked).map(e => `${e.name}=${e.value}`).join('&')
-    fetch(`/articlecount?nsq=${namespaceSearchQuery}&${namespaces}`).then(response => {
-      if (response.ok) {
-        return response.text()
-      }
-      throw new Error(`${response.status} ${response.statusText}`)
-    }).then(body => {
-      document.getElementById('namespace-filters').innerHTML = body
-    }).catch((err) => {
-      causeAlert(err.message)
-
-      for (const n of document.querySelectorAll('[name="n"]')) {
-        n.removeAttribute('disabled')
-        n.parentElement.classList.remove('placeholder-glow')
-        document.querySelector(`[for="${n.id}"]`).classList.remove('placeholder')
-      }
-    })
-  })
+  namespacesForm.addEventListener('submit', forms.namespaces)
   namespaceDiv.replaceWith(namespacesForm)
 
   // override search form behaviour
   const search = document.getElementById('search')
-  search.addEventListener('submit', function(event) {
-    event.preventDefault()
-
-    const formdata = new FormData(search)
-    const params = Array.from(formdata).map(f => f.join('=')).join('&')
-
-    // update the browser history
-    history.pushState({}, '', `/search?${params}`)
-
-    // set the placeholders
-    for (const e of document.querySelectorAll('.twikilink')) {
-      e.classList.add('placeholder', 'disabled')
-      e.ariaDisabled = true
-      e.setAttribute('tabindex', -1)
-      e.parentElement.classList.add('placeholder-glow')
-    }
-
-    // submit the form
-    fetch(`/results?${params}`).then((response) => {
-      if (response.ok) {
-        return response.text()
-      }
-
-      throw new Error(`${response.status} ${response.statusText}`)
-    }).then((body) => {
-      document.getElementById('results').innerHTML = body
-    }).catch((err) => {
-      causeAlert(err.message)
-
-      // remove the placeholders
-      for (const e of document.querySelectorAll('.twikilink')) {
-        e.classList.remove('placeholder', 'disabled')
-        e.ariaDisabled = false
-        e.removeAttribute('tabindex')
-        e.parentElement.classList.remove('placeholder-glow')
-      }
-    })
-  })
+  search.addEventListener('submit', forms.search)
 
   // reload trope filters if namespaces are change
   document.querySelectorAll('[name="n"]').forEach(e => {
@@ -134,18 +37,3 @@ window.onload = function() {
   })
 }
 
-const causeAlert = (message) => {
-  const alert = document.createElement('div')
-  alert.classList.add('alert', 'alert-warning', 'alert-dismissible', 'fade', 'show')
-  alert.innerHTML = message
-  alert.role = "alert"
-
-  const closeButton = document.createElement('button')
-  closeButton.type = "button"
-  closeButton.classList.add("btn-close")
-  closeButton.dataset.bsDismiss = "alert"
-  closeButton.ariaLabel = "Close"
-
-  alert.appendChild(closeButton)
-  document.body.insertBefore(alert, document.body.firstChild)
-}
